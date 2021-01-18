bcc4d4daea62fe76c2b1871eb7028be89a80f836
yjhoo <yjhoorang@gmail.com>
2021-01-18 10:41:08 +0900

    Update 3. 부분집합 구하기_yoo.py

--- section6/3. 부분집합 구하기_yoo.py
+++ section6/3. 부분집합 구하기_yoo.py
@@ -2,12 +2,10 @@
 import sys
 import math
 from collections import deque
-# heapq를 사용할 수 있도록 추가
 import heapq as hq
 
 #sys.stdin = open("in1.txt", "rt")
 
-# 이진수로 만들어주는 함수
 def f_DFS(v):
     if v == N + 1:
         for i in range(1, N + 1):
