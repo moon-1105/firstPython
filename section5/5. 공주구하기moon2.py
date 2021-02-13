e0140196e6ac8a6f28a06d3efa3dea9a92258256
moon-1105 <moonkite1105@gmail.com>
2021-02-14 03:31:00 +0900

    이게찐임

--- section5/5. 공주구하기moon2.py
+++ section5/5. 공주구하기moon2.py
@@ -1,24 +1,18 @@
 import sys
 sys.stdin=open("input.txt", "r")
-
+from collections import deque
 
 # 1개만 남을때까지 항상 K 번째 수(K의 배수)가 제거됨.
 N, K = map(int, input().split())
+dq = list(range(1, N+1))
+dq = deque(dq)
 
-q = []
-for idx in range(1, N+1) :
-    q.append(idx)
-
-chk = 0
-while len(q) != 1:
-    if idx == len(q):
-        idx = 0
-    chk+=1
-    # idx 로 체크했더니 ...?
-    if chk % K == 0:
-        q.remove(q[idx])
-        #print(q)
-    else:
-        idx+=1
-
-print(q[0])
+while dq:
+    for _ in range(K-1): #아무 변수 없이 반복
+        cur = dq.popleft() # 맨 앞에거가 pop
+        dq.append(cur) # 맨 뒤로 붙이기
+    #이렇게 해서 K번 자리를 옮기게 하고
+    dq.popleft()
+    if len(dq) == 1:
+        print(dq[0])
+        dq.popleft()
