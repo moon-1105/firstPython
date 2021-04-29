09ac323dcdc954df7f42503494c49768523115ae
moon-1105 <moonkite1105@gmail.com>
2021-04-30 00:05:52 +0900

    됐는데 왜 되었지...우선 돌고, 팝한 뒤쪽에서 또 돌고?

--- section6/8.순열구하기_moon.py
+++ section6/8.순열구하기_moon.py
@@ -2,27 +2,33 @@
 1부터 N까지 번호가 적힌 구슬이 있습니다. 이 중 M개를 뽑아 일렬로 나열하는 방법을 모두
 출력합니다.
 """
-"""
+
 N, M = map(int, input().split())
 """
-N, M = 5, 3
+N, M = 3, 2
+"""
 marv = []
 for num in range(1, N+1):
     marv.append(num)
 ans_arr = []
-
-def cal(arr, idx, m_idx, 개수):
-    if M == 개수:
+lastNum = 0
+def cal(arr):
+    if M == len(arr):
         # 답인 경우
-        newArr = arr[:]
-        global ans_arr
-        ans_arr.append(newArr)
-        #print(arr)
+        for ele in arr:
+            print(ele, end=" ")
+        global lastNum
+        lastNum+=1
+        print()
         return
-    if  idx >= M or m_idx >= N or M < 개수:
-        return
-    arr[idx]= marv[m_idx]
-    cal(arr, idx+1, m_idx+1, 개수+1)
-    cal(arr, idx, m_idx+1, 개수)
+    for num in range(1, N+1):
+        if not(num in arr):
+            arr.append(num)
+            cal(arr)
+            arr.pop()
 
-cal([0]*M,0,0,0)
+for num in range(1, N+1):
+    startArr = [ num ]
+    cal(startArr)
+
+print(lastNum)
