9c76c9ce21a330a3fe3d8bf23daec2153bb01e37
moon-1105 <moonkite1105@gmail.com>
2021-05-12 21:57:54 +0900

    깔끔..

--- section7/8. 사과나무_moon.py
+++ section7/8. 사과나무_moon.py
@@ -1,12 +1,13 @@
 import sys
 from collections import deque
-sys.stdin = open("in.txt","r")
+#sys.stdin = open("in.txt","r")
 
 N = int(input())
 tree = []
+his = []
 for _ in range(N):
     tree.append(list(map(int, input().split())))
-his = [[0]*N]*N
+    his.append([0]*N)
 
 dir = [[1,0], [0,1], [-1,0], [0,-1]]
 apple = 0
@@ -14,21 +15,26 @@ now = [N//2, N//2]
 
 q = deque()
 q.append(now)
-for _ in range( N//2+1 ):
-    now = q.pop()
-    for d in dir:
-        if 0<= now[0]+d[0] < N and 0<= now[1]+d[1] < N:
-            next = [ now[0]+d[0], now[1]+d[1] ]
-            print(next)
-            if his[next[0]][next[1]] == 0:
-                print("hi")
-                q.append(next)
-                apple += tree[next[0]][next[1]]
-                his[next[0]][next[1]] = 1
-    print("=============")
-    for ele in his:
-        print(ele)
-    print("=============")
+his[now[0]][now[1]] = 1
+apple += tree[now[0]][now[1]]
+for _ in range( N//2 ):
+    newq = deque()
+    while q: # 안에 있으면...
+        now = q.pop()
+        # 방향으로 진행 가능하면 진행
+        for d in dir:
+            if 0<= now[0]+d[0] < N and 0<= now[1]+d[1] < N:
+                next = [ now[0]+d[0], now[1]+d[1] ]
+                if his[next[0]][next[1]] == 0:
+                    newq.append(next)
+                    apple += tree[next[0]][next[1]]
+                    his[next[0]][next[1]] = 1
+    q = newq.copy()
+   # print("=============")
+    #for ele in his:
+    #    print(ele)
+    #print("=============")
+
 print(apple)
 
 
