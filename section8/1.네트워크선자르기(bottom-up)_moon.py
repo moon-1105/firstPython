400984be8b5bfd976abfaa5e1278c131aac6e26a
moon-1105 <moonkite1105@gmail.com>
2021-05-23 22:57:50 +0900

    경우의 수를 구하는게 포인트이므로 => 해당 길이 만큼의 경우의 수 = 이전 경우의 수 + 그 이전 경우의 수 최종 경우의 수

--- section8/1.네트워크선자르기(bottom-up)_moon.py
+++ section8/1.네트워크선자르기(bottom-up)_moon.py
@@ -1,25 +1,16 @@
 import sys
-#sys.stdin = open("in.txt","r")
+sys.stdin = open("in.txt","r")
 
 #네트워크 선을 1m, 2m의 길이를 갖는 선으로 자르려고 합니다
 #네트워크 선의 길이가 Nm라면 몇 가지의 자르는 방법을 생각할 수 있나요?
 
-cnt = 0
-def cal(total):
-    global cnt
-    if total >= N:
-        if total > N:
-            return
-        else:
-          #  print("성공")
-            cnt+=1
-            return
-    cal(total+1)
-    cal(total+2)
-
 if __name__ == "__main__":
     N = int(input())
-    cal(0)
-    print(cnt)
+    L = [0]*N
+    L[0], L[1] = 1, 2
+    for i in range(2, N):
+        L[i] = L[i-1]+L[i-2]
+
+    print(L[N-1])
 
 
