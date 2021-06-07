1d2b36e46bea526d32b2a5dfea5eb11a55adffb7
moon-1105 <moonkite1105@gmail.com>
2021-06-07 17:28:05 +0900

    ans[i] 에는 i kg 까지 담았을 때의 최대의 가치

--- section8/9.가방문제_moon.py
+++ section8/9.가방문제_moon.py
@@ -1,25 +1,31 @@
 import sys
-sys.stdin = open("in.txt","r")
+#sys.stdin = open("in.txt","r")
 """
 이 보석을 가방에 담는데 XX kg를 넘지 않으면서 
 최대의 가치가 되도록 하려면 어떻게 담아야 할까요?
 """
-class jewel:
-    def __init__(self, value, weight):
-        self.value = value
-        self.weight = weight
-    def __str__(self):
-        return "가치:"+str(self.value)+" / "+"무게: "+str(self.weight)
 
-if __name__ == "__init__":
+if __name__ == "__main__":
     N, limit = map(int, input().split())
     arr = []
     for _ in range(N):
-        w, v = map(int, input().split())
-        arr.append(jewel(v, w))
-    ans = [0]*N
-    ans[0] = max(arr)
-    for i in range(0,N)
+        arr.append(list( map(int, input().split())))
+    """
+    ans[i] 에는 i kg 까지 담았을 때의 최대의 가치
+    무게별로 체크해가면서 최대의 가치를 체크. 
+    """
+    ans = [0] * (limit+1)
+    for ele in arr:
+        무게, 가치 = ele[0], ele[1]
+        for i in range(무게, limit+1):
+            if ans[i] < ans[i-무게] + 가치:
+                ans[i] = ans[i-무게] + 가치
+        #print("무게:",무게, ans)
+
+    print(max(ans))
+
+
+
 
 
 
