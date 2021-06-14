5a0917e60d35a965646b33c2285a7698ccf74324
moon-1105 <moonkite1105@gmail.com>
2021-06-14 23:56:51 +0900

    푸는중,,

--- section8/12. 플로이드 워샬_moon.py
+++ section8/12. 플로이드 워샬_moon.py
@@ -20,6 +20,12 @@ def findShort(idx):
         return m
     else:
         return 0
+def printMap():
+    for i in range(1, city+1):
+        for j in range(1, city+1):
+            print('{0:2d}'.format(ans[i][j]), end=" ")
+        print()
+    print("=====================")
 
 if __name__ == "__main__":
     city, road = map(int, input().split())
@@ -28,23 +34,23 @@ if __name__ == "__main__":
     for _ in range(road):
         arr.append(list(map(int, input().split())))
         # [도시1, 도시2, 비용]
+    #   INIT
     for _ in range(city+1):
-        ans.append(['M']*(city+1))
+        ans.append([50]*(city+1))
+    for ele in arr:
+        ans[ele[0]][ele[1]] = ele[2]
 
     for i in range(1, city+1):
         for j in range(1, city+1):
             # i 에서 j로
             # j로 가는 비용 + [i까지 가장 짧은 길]
-            v = canGo(i, j)
-            if  v != -1 :
-                ans[i][j] = findShort(i) + v
-            else:
-                ans[i][j] = 'M'
-        for ele in ans:
-            print(ele[1:])
-        print("=====================")
+            ans[i][j] = min(findShort(i), ans[i][j])
+
+        printMap()
+
     for i in range(1, city + 1):
         ans[i][i] = 0
+
 
 
 
