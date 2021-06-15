cb04e531567ede4aae3cf1a6989c21aaaf58d429
moon-1105 <moonkite1105@gmail.com>
2021-06-16 00:41:08 +0900

    거의 되었는데 좀 다름,,,,,,

--- section8/12. 플로이드 워샬_moon.py
+++ section8/12. 플로이드 워샬_moon.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open("in.txt")
+#sys.stdin = open("in.txt")
 """
 모든 도시에서 모든 도시로 이동하는데 쓰이는 비용의 최소값
 """
@@ -9,7 +9,16 @@ def printMap():
         for j in range(1, city+1):
             print(ans[i][j], end=" ")
         print()
-    print("=====================")
+
+def findMin(A, B):
+    if A == 'M' and 'M' in B:
+        return 'M'
+    elif A == 'M':
+        return B[0]+B[1]
+    elif 'M' in B:
+        return A
+    else:
+        return min(A, B[0]+B[1])
 
 if __name__ == "__main__":
     city, road = map(int, input().split())
@@ -25,12 +34,15 @@ if __name__ == "__main__":
         ans[ele[0]][ele[1]] = ele[2]
     for i in range(1, city + 1):
         ans[i][i] = 0
-    printMap()
+    #printMap()
 
     # ans[i][j] = ans[i][k] + ans[k][j] => 중에 가장 작은값으로 계속 업데이트함.
-    for idx in range(1, city+1):
+    for i in range(1, city+1):
+        for j in range(1, city+1):
+            for idx in range(1, city+1):
+                ans[i][j] = findMin(ans[i][j], [ans[i][idx] , ans[idx][j]])
 
-
+    printMap()
 
 
 
